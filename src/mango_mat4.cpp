aa516e0b40d91a02a854360f2bbe365833ba582b
t0rakka <jukka@liimatta.org>
2026-05-07 16:03:34 +0300

    Add mango mat+mat test.

--- src/mango_mat4.cpp
+++ src/mango_mat4.cpp
@@ -5,6 +5,19 @@
 
 using float4x4 = mango::math::Matrix4x4;
 
+static void mat4_add(benchmark::State& state) {
+    const auto testData = prepare_mat4_test_data<float4x4>(state.range(0));
+
+    float4x4 res(0.0f);
+
+    for (auto _ : state) {
+        benchmark::ClobberMemory();
+        res = testData[0] + testData[1];
+        benchmark::ClobberMemory();
+    }
+    benchmark::DoNotOptimize(res);
+}
+
 static void mat4_mult(benchmark::State& state) {
     const auto testData = prepare_mat4_test_data_mango<float4x4>(state.range(0));
 
@@ -47,6 +60,7 @@ static void mat4_mult_loop_accumulate(benchmark::State& state) {
     benchmark::DoNotOptimize(res);
 }
 
+BENCHMARK(mat4_add)->Arg(2);
 BENCHMARK(mat4_mult)->Arg(2);
 BENCHMARK(mat4_mult_loop)->Arg(2)->Arg(8)->Arg(64)->Arg(1 << 10);
 BENCHMARK(mat4_mult_loop_accumulate)->Arg(2)->Arg(8)->Arg(64)->Arg(1 << 10);
